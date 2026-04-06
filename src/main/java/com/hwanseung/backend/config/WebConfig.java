51d1d1a270a8d34ce7c10d0e64c8d7d2ad7e1695
essong915 <essong915@gmail.com>
2026-04-06 09:20:27 +0900

    0406_start

--- src/main/java/com/hwanseung/backend/config/WebConfig.java
+++ src/main/java/com/hwanseung/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.hwanseung.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,19 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private String filePath = "file:///C:/bImg/";
+    @Value("${custom.upload-path}")
+    private String filePath ;
 
     @Override  // 리소스 외부 경로 맵핑 설정
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (!os.contains("win")) {
-            filePath = "/Users/ikarosala/Documents/bImg/";
-        }
-
         registry.addResourceHandler("/api/imgs/**") // 웹에서 접근할 경로
-                .addResourceLocations(filePath); // 실제 파일이 있는 위치
+                .addResourceLocations("file:///"+filePath); // 실제 파일이 있는 위치
         registry.addResourceHandler("/api/download/**")
-                .addResourceLocations(filePath);
+                .addResourceLocations("file:///"+filePath);
     }
 
 
@@ -30,7 +27,9 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3000",
                         "http://127.0.0.1:3000",
                         "http://localhost:5173",
-                        "http://127.0.0.1:5173"
+                        "http://127.0.0.1:5173",
+                        "http://15.164.62.197",
+                        "http://hwanseung.kro.kr"
                 )
                 .allowCredentials(true) // 중요!
                 .allowedMethods("*");
